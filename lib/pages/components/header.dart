3d749c9cd6b02907c4707870897b22fa9bf9fedb
Elberte Plinio <elbertedoolan@gmail.com>
2022-12-06 11:35:52 -0300

    header items

--- lib/pages/components/header.dart
+++ lib/pages/components/header.dart
@@ -1,6 +1,7 @@
 import 'package:elberte_com/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import '../../models/header_item.dart';
 
@@ -66,6 +67,68 @@ class HeaderLogo extends StatelessWidget {
   }
 }
 
+class HeaderRow extends StatelessWidget {
+  const HeaderRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveVisibility(
+      visible: false,
+      visibleWhen: const [
+        Condition.largerThan(name: MOBILE),
+      ],
+      child: Row(
+        children: headerItems
+            .map(
+              (item) => item.isButton
+                  ? MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: kDangerColor,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 5,
+                        ),
+                        child: TextButton(
+                          onPressed: item.onTap,
+                          child: Text(
+                            item.title,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 13,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  : MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 30),
+                        child: GestureDetector(
+                          onTap: item.onTap,
+                          child: Text(
+                            item.title,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 13,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
 class Header extends StatelessWidget {
   const Header({super.key});
 
@@ -77,6 +140,7 @@ class Header extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: const [
           HeaderLogo(),
+          HeaderRow(),
         ],
       ),
     );
