4f8ab96a278f33dcffc6ca5ba4978ac0226b5f13
Elberte Plinio <elbertedoolan@gmail.com>
2022-12-08 20:37:21 -0300

    add firebase for auto deploy

--- lib/pages/home/components/home_sections.dart
+++ lib/pages/home/components/home_sections.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../components/components.dart';
 
+//TODO: Add download curriculum
 final List<Widget> homeSections = [
   // const HeaderSection(),
   const Padding(
